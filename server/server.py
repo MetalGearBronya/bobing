c239f4268b969ae49f77b7bbbd7e533658362c66
Yokissa <amoyhuangzheng@yahoo.com>
2022-08-14 03:01:19 +0800

    change to websocket server

--- server/server.py
+++ server/server.py
@@ -1,62 +1,26 @@
-#!/usr/bin/env python3
-"""
-Very simple HTTP server in python for logging requests
-Usage::
-    ./server.py [<port>]
-"""
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import logging
-import json
-import random
+from websocket_server import WebsocketServer
 
-# 服务部分
-class S(BaseHTTPRequestHandler):
-    def _set_response(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-
-    def do_GET(self):
-        if self.path == '/roll':
-            logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
-            self._set_response()
-            # self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
-            spin_duration = [1.5, 2, 2.5, 3, 3.5]
-            obj = {"num" : random.randint(1, 6), "timeout" : int(random.choice(spin_duration) * 1000)}
-            self.wfile.write(json.dumps(obj).encode('utf-8'))
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                     str(self.path), str(self.headers), post_data.decode('utf-8'))
-
-        self._set_response()
-        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
-
-        # 后台操作写在这里
-        # with open('result.csv', 'a') as output:
-        #     output.write('{}\t{}\t{}\n'.format(current_index, entities, correct))
-
-def run(server_class=HTTPServer, handler_class=S, port=8080):
-    logging.basicConfig(level=logging.INFO)
-    server_address = ('', port)
-    httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+	print("New client connected and was given id %d" % client['id'])
+	server.send_message_to_all("Hey all, a new client has joined us")
 
 
-if __name__ == '__main__':
+# Called for every client disconnecting
+def client_left(client, server):
+	print("Client(%d) disconnected" % client['id'])
 
-    from sys import argv
-    # 允许手动声明端口
-    if len(argv) == 2:
-        run(port=int(argv[1]))
-    else:
-        run()
+
+# Called when a client sends a message
+def message_received(client, server, message):
+	if len(message) > 200:
+		message = message[:200]+'..'
+	print("Client(%d) said: %s" % (client['id'], message))
+
+
+PORT=9001
+server = WebsocketServer(port = PORT)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
